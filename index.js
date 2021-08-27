f5b45a58c009c9615d76b13ec82d3a514b9319df
Lawrence J <lawrenceg563@gmail.com>
2021-08-27 09:05:06 +0530

    created a basic form with action and method to simulate the request and serve content

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method);
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  if (req.url === "/") {
     res.write("<html>");
     res.write("<head><title>Basics testing</title></head>");
-    res.write("<body><h1>Hello it works we see this content from node http server</h1></body>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">click</button></form></body>'
+    );
     res.write("</html>");
-    res.end()
-})
+    return res.end();
+  }
+  res.write("<html>");
+  res.write("<head><title>Basics testing</title></head>");
+  res.write(
+    "<body><h1>Hello it works we see this content from node http server</h1></body>"
+  );
+  res.write("</html>");
+  res.end();
+});
 server.listen(3000);
