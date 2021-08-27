6e5d57e260b54f4c3964be6eec3ab5ea8cab6269
Lawrence J <lawrenceg563@gmail.com>
2021-08-27 15:13:35 +0530

    added a sepearate html 404 page not found file and linked in index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -9,5 +10,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
 
 app.listen(3000)
