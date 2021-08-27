8751de435a1f06dc6e692d01ef2599289f7e8fd3
Lawrence J <lawrenceg563@gmail.com>
2021-08-27 08:05:10 +0530

    basic response with write function is implemented

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
     console.log(req.url, req.method);
+    res.write("<html>");
+    res.write("<head><title>Basics testing</title></head>");
+    res.write("<body><h1>Hello it works we see this content from node http server</h1></body>");
+    res.write("</html>");
+    res.end()
 })
 server.listen(3000);
