e46948ecbf27a92709eed64dba21907910a7cc0a
Lawrence J <lawrenceg563@gmail.com>
2021-08-27 13:49:03 +0530

    added express and bodyparser and get user input and display in console using middleware

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  if (req.url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Basics testing</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">click</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  res.write("<html>");
-  res.write("<head><title>Basics testing</title></head>");
-  res.write(
-    "<body><h1>Hello it works we see this content from node http server</h1></body>"
-  );
-  res.write("</html>");
-  res.end();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+res.send('<form action="/product" method="POST"><input type="text" name="message"><button type="submit">click</button></form>')
 });
-server.listen(3000);
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello there</h1>')
+});
+app.listen(3000)
