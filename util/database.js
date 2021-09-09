7a1be950cb58d6f6b17486a18d1abe75d31fe4ef
Lawrence J <lawrenceg563@gmail.com>
2021-09-09 19:01:17 +0530

    omitting sequelize learning path due to time constraints and leaving sequelize models for future reference continue in noSQL mongo db path from now on

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require("sequelize");
+require('dotenv').config()
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "nodecomplete", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = callback => {
+  MongoClient.connect(process.env.MONGO_URI)
+  .then(client=>{
+    console.log('connected!');
+    callback(client);
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
