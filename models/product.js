d769e465f3660ab0d3c53183c2beb0b212625abf
Lawrence J <lawrenceg563@gmail.com>
2021-09-09 20:36:55 +0530

    created fetchById method changed ejs vies to adapt new _id of mongoDb

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -30,7 +31,22 @@ class Product {
         console.log(products);
         return products;
       })
-      .catch(err => {
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
         console.log(err);
       });
   }
